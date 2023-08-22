e5f9a19c179c888c9e8e4730ffa85c7f35f30006
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2023-08-22 10:26:27 -0300

    .

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <span>Addmin</span>
+            <span>Addmin DashBoard Controller</span>
+        </div>
+    )
+}
+
+export default Footer
